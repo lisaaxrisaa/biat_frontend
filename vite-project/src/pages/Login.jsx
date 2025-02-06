ed639d64b332a196d69005c5a7d26e5163088f1e
Nicole Riera <150852807+NicoRi5@users.noreply.github.com>
2025-02-06 11:28:26 -0500

    Fix login form

--- vite-project/src/pages/Login.jsx
+++ vite-project/src/pages/Login.jsx
@@ -1,58 +1,44 @@
 // changes made:
-// - replaced mock token with api token
-// - add isLoading for button, keeps users from double submitting and lets them know it is processing
-// - improved error handling
+// import form component
+// replace current form with new (bootstrap) form component
+// remove const email and password, which are no longer needed
 
-import { useState } from 'react';
-import { useLoginUserMutation } from '../store/loginSlice';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { registerUser } from '../store/authSlice';
+import { useState } from "react";
+import { useLoginUserMutation } from "../store/loginSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { registerUser } from "../store/authSlice";
+import Form from "./Form";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [error, setError] = useState(null);
   const [loginUserApi, { isLoading }] = useLoginUserMutation();
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e, email, password) => {
+    // add email and password since they are no longer declared
     e.preventDefault();
     try {
       const credentials = { email, password };
       const response = await loginUserApi(credentials).unwrap();
       dispatch(registerUser({ token: response.token, user: response.user }));
-      navigate('/');
+      navigate("/");
     } catch (error) {
-      setError('Invalid login, please try again!');
-      console.error('Invalid login, please try again!', error);
+      setError("Invalid login, please try again!");
+      console.error("Invalid login, please try again!", error);
     }
   };
 
   return (
     <>
       <h2>Login</h2>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit" disabled={isLoading} className="btn btn-primary">
-          {isLoading ? 'Loading...' : 'Login'}
-        </button>
-      </form>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <Form
+        formType="login"
+        handleSubmit={handleSubmit}
+        isLoading={isLoading}
+      />
     </>
   );
 };

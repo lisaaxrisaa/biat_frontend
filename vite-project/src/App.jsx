2bafd4b0b8d12c7914ebda0540536ace1c6079c2
Lisa Fujita <lisafujita90@gmail.com>
2025-02-12 10:35:22 -0500

    Added CSS styling to edit itinerary page

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,18 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Registration from "./pages/Registration";
-import UpdateProfile from "./pages/UpdateProfile";
-import Profile from "./pages/Profile";
-import NavBar from "../../NavBar";
-import ItineraryPage from "./Itinerary/ItineraryPage";
-import ItineraryForm from "./Itinerary/ItineraryForm";
-// add JournalList import and Route Path
-import JournalList from "./journal-components/JournalList";
-import { Provider } from "react-redux";
-import store from "./store/store";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Registration from './pages/Registration';
+import UpdateProfile from './pages/UpdateProfile';
+import Profile from './pages/Profile';
+import NavBar from '../../NavBar';
+import ItineraryPage from './Itinerary/ItineraryPage';
+import ItineraryForm from './Itinerary/ItineraryForm';
+import ItineraryDetailPage from './Itinerary/ItineraryDetail';
+import EditItineraryPage from './Itinerary/EditItinerary';
+import JournalList from './journal-components/JournalList';
+// import Logout from './pages/Logout';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const App = () => {
   return (
@@ -24,11 +26,15 @@ const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="/itineraries" element={<ItineraryPage />} />
             <Route path="/itinerary-form" element={<ItineraryForm />} />
+            <Route path="/itinerary/:id" element={<ItineraryDetailPage />} />
+            <Route path="/edit-itinerary/:id" element={<EditItineraryPage />} />
+
             <Route path="/journals" element={<JournalList />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Registration />} />
             <Route path="/update-profile" element={<UpdateProfile />} />
             <Route path="/profile" element={<Profile />} />
+            {/* <Route path="/logout" element={<Logout />} /> */}
           </Routes>
         </BrowserRouter>
       </Provider>

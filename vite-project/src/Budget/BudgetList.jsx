1d9e2e50088c16fead6ee3b30213149a56d562a5
Nicole Riera <150852807+NicoRi5@users.noreply.github.com>
2025-02-19 13:49:40 -0500

    Create skeleton files for budget component

--- vite-project/src/Budget/BudgetList.jsx
+++ vite-project/src/Budget/BudgetList.jsx
@@ -1,1 +1,51 @@
 // this file should list all budgets, only overviews not the information.
+
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const BudgetList = () => {
+  const budgets = useSelector((state) => state.budget.budgets);
+  const location = useLocation;
+
+  useEffect(() => {
+    // refetch if needed, like in journalList.jsx
+  }, [location]);
+
+  const handleViewClick = (budgetId) => {
+    history.push(`/budget/${budgetId}`);
+  };
+  const renderBudgets = () => {
+    if (!budgets || budgets.length === 0) {
+      return (
+        <div>
+          {" "}
+          <p>No budgets available</p>
+          <Link to="/create-budget">
+            <button>Create a New Budget</button>
+          </Link>
+        </div>
+      );
+    }
+    return budgets.map((budget) => (
+      <div key={budget.id}>
+        <h3>{budget.name}</h3>
+        <p>{budget.description}</p>
+        <Link to={`/budget/${budget.id}`}>
+          <button>View Budget</button>
+        </Link>
+      </div>
+    ));
+  };
+  return (
+    <div>
+      <h2>Your Budgets</h2>
+      <Link to="/create-budget">
+        <button>Create New Budget</button>
+      </Link>
+      {renderBudgets()}
+    </div>
+  );
+};
+
+export default BudgetList;
--- vite-project/src/Budget/IndividualBudget.jsx
+++ vite-project/src/Budget/IndividualBudget.jsx
@@ -1,2 +1,28 @@
 // this file should display a specific budget table
 // with the option to edit, and save it or delete it
+
+import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+// import edit budget
+// import delete budget
+// import budget form
+
+const IndividualBudget = () => {
+  const { budgetId } = useParams();
+  const budget = useSelector((state) =>
+    state.budget.budgets.find((budget) => budget.id === budgetId)
+  );
+  if (!budget) return <p>Budget not found</p>;
+  return (
+    <div>
+      <h2>{budget.name}</h2>
+      {/* <p></p> */}
+      {/* <p></p> */}
+      {/* <BudgetForm budgetId={budgetId}/> */}
+      {/* editbudget */}
+      {/* deletebudget */}
+    </div>
+  );
+};
+export default IndividualBudget;
